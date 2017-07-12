328c0c694a3018445a30ce86e27fa9831e3bf3b0
Ibrahim5697 <ibrahim.jece@ksrce.ac.in>
2017-07-12 10:44:09 +0530

    Add files via upload

---
+++ upperndlower206.java
@@ -0,0 +1,17 @@
+package player;
+import java.util.Scanner;
+public class upperndlower206 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in); 
+		String s=sc.nextLine();
+		char[] ch=s.toCharArray();
+		for(int i=0;i<s.length();i++) {
+		if(Character.isLowerCase(ch[i]))
+		System.out.print(Character.toUpperCase(ch[i]));
+		else
+		System.out.print(Character.toLowerCase(ch[i]));
+		}
+	}
+
+}
