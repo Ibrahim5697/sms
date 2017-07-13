6848d018643a873d01f6010d527faa781f49c021
Ibrahim5697 <ibrahim.jece@ksrce.ac.in>
2017-07-13 09:59:06 +0530

    Add files via upload

---
+++ si192.java
@@ -0,0 +1,15 @@
+package player;
+import java.util.Scanner;
+public class si192 {
+
+	public static void main(String[] args) {
+		Scanner sn=new Scanner(System.in);
+		int p=sn.nextInt();
+		int n=sn.nextInt();
+		float r=sn.nextFloat();
+		double x=(p*n*r)/100;
+		System.out.println(x);
+
+	}
+
+}
