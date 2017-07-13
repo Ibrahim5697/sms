954c0f8b4a882d63d67f21ce84a431065c011139
Ibrahim5697 <ibrahim.jece@ksrce.ac.in>
2017-07-13 11:03:34 +0530

    Add files via upload

---
+++ player170.java
@@ -0,0 +1,11 @@
+package player;
+
+public class player170 {
+
+	public static void main(String[] args) {
+		String a="this is a beautiful place to get tech questions";
+		String b=a.replaceAll("[ise]","");
+         System.out.println(b);
+	}
+
+}
