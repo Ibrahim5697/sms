2eadcc93255bf380e496a56022d17252c45578e5
Ibrahim5697 <ibrahim.jece@ksrce.ac.in>
2017-07-13 09:34:40 +0530

    Add files via upload

---
+++ star132.java
@@ -0,0 +1,23 @@
+package hunter35;
+import java.util.Scanner;
+public class star132 {
+
+	public static void main(String[] args) {
+		{
+			Scanner ss=new Scanner(System.in);
+			int n=ss.nextInt();
+			int n0=n;
+			for(int i=0;i<n;i++) {
+			for(int j=0;j<n0;j++) {
+			System.out.print("*");
+			}
+			System.out.print(" ");
+			for(int k=0;k<n0;k++) {
+			System.out.print("*");
+			}
+			System.out.print("\n");
+			n0--;
+			}
+	}
+	}
+}
